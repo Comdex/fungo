fcd465872792f440e351a83a66035d52fe5da343
Comdex <wcomdex@foxmail.com>
2014-07-21 20:26:53 +0800

    update something about encoding

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ package main
 import (
 	"fmt"
 	"github.com/codegangsta/cli"
-	iconv "github.com/djimenez/iconv-go"
+	//iconv "github.com/djimenez/iconv-go"
 	"os"
 )
 
@@ -15,22 +15,23 @@ func main() {
 	app.Version = "0.1.0"
 
 	app.Flags = []cli.Flag{
-		cli.StringFlag{Name: "new", value: "网站目录名", Usage: `"fungo new 网站目录名"命令会在fungo所在的当前目录下生成一个静态网页目录内含样式文件等`},
+		cli.StringFlag{Name: "new", Value: "网站目录名", Usage: `"fungo new 网站目录名"命令会在fungo所在的当前目录下生成一个静态网页目录内含样式文件等`},
 	}
 
 	app.Action = func(c *cli.Context) {
 		//全局帮助提示
 		message := "Please use command \"fungo -h\" or \"fungo --help\" get information for help \n请使用命令 \"fungo -h\" 或 \"fungo --help\" 获取帮助的信息"
-		cmessage, _ := iconv.ConvertString(message, "UTF-8", "GBK")
+		//ms, _ := iconv.ConvertString(message, "UTF-8", "GBK")
 
 		//分析全局选项命令
-		var dirName string
+		//var dirName string
 		if len(c.Args()) > 0 {
-			dirName = c.Args()[0]
+			//dirName = c.Args()[0]
 
 		}
-
-		fmt.Println(cmessage)
+		//path, _ := os.Getwd()
+		fmt.Println(message)
+		//fmt.Println(path)
 	}
 	app.Commands = []cli.Command{
 		{
