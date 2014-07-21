709069f26d044f9ee261dded90d36a264f088044
Comdex <wcomdex@foxmail.com>
2014-07-21 22:42:16 +0800

    add the fungo new function

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"fmt"
 	"github.com/codegangsta/cli"
+	//"github.com/astaxie/beego/config"
 	"os"
 )
 
@@ -13,6 +14,7 @@ func main() {
 	app.Usage = "fungo is a static website generator by Comdex \n   fungo 是一个静态网站生成器 by Comdex"
 	app.Version = "0.1.0"
 
+	//全局选项
 	app.Flags = []cli.Flag{
 		cli.StringFlag{Name: "new", Value: "网站目录名", Usage: `"fungo new 网站目录名"命令会在fungo所在的当前目录下生成一个静态网页目录内含样式文件等`},
 	}
@@ -22,14 +24,72 @@ func main() {
 		message := "Please use command \"fungo -h\" or \"fungo --help\" get information for help \n请使用命令 \"fungo -h\" 或 \"fungo --help\" 获取帮助的信息"
 
 		//分析全局选项命令
-		//var dirName string
 		if len(c.Args()) > 0 {
-			//dirName = c.Args()[0]
+			cliName := c.Args()[0]
+			//获取目录名
+			if cliName == "new" {
+				dirName := c.Args()[1]
+				if dirName != "" {
+					//获取当前路径并在此路径下创建目录
+					//path,_:=os.Getwd()
+					err := os.Mkdir(dirName, 0777)
+					checkError(err)
+					err1 := os.MkdirAll(dirName+`/static/css`, 0777)
+					checkError(err1)
+					err2 := os.MkdirAll(dirName+`/static/js`, 0777)
+					checkError(err2)
+					err3 := os.MkdirAll(dirName+`/static/img`, 0777)
+					checkError(err3)
+					//写入配置文件
+					fout, err4 := os.Create(dirName + "/site.conf")
+					checkError(err4)
+					defer fout.Close()
 
+					_, err5 := fout.WriteString(`[global]
+#博客/网站名字
+title = "fungo"
+#网站介绍
+introduce = "fungo"
+#网站地址
+url = "www.example.com"
+#网站作者
+author = "fungo"
+#网站关键字
+keywords = "fungo"
+#作者邮箱 
+email = "fungo@fungo.com"
+[personal]
+#首页显示文章条数
+latest = 6
+#首页文章摘要长度
+summary = 50`)
+					checkError(err5)
+
+					//写入演示文章
+					fout, err6 := os.Create(dirName + "/1.article")
+					checkError(err6)
+					defer fout.Close()
+
+					_, err7 := fout.WriteString(`#文章标题
+title：大家好,这是我的第一篇博客！
+#标签
+tag:大家好
+#文章网页url,可选，默认为文件名
+article-url:dajiahao.html
+#内容
+content:
+这是我的第一篇博客！请大家多多支持啊，fungo是一个非常有趣的静态网页生成引擎，欢迎使用和提出建议!`)
+					checkError(err7)
+				}
+
+			}
+
+		} else {
+			fmt.Println(message)
+			path, _ := os.Getwd()
+			fmt.Println(path)
 		}
-		//path, _ := os.Getwd()
-		fmt.Println(message)
-		//fmt.Println(path)
+
 	}
 	app.Commands = []cli.Command{
 		{
@@ -72,4 +132,14 @@ func main() {
 	}
 
 	app.Run(os.Args)
+}
+
+//错误检测函数
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	} else {
+		fmt.Println("Successful Executing!")
+	}
 }
